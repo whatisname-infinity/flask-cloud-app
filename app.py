4faf7857f97f21210bd2094334b2b497d9df1a34
amit <amit@chavle.com>
2026-01-17 16:58:55 +0000

    Added psutil for real-time server CPU and RAM monitoring

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import datetime
 from datetime import timedelta
+import psutil
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ def home():
     now_utc = datetime.datetime.now()
     now_ist = now_utc + timedelta(hours=5, minutes=30)
     current_time = now_ist.strftime("%H:%M:%S")
-    return render_template('index.html',user_name="Amit", server_time=current_time)
+    cpu_percent = psutil.cpu_percent(interval=None)
+    mem_percent = psutil.virtual_memory().percent
+    return render_template('index.html',user_name="Amit", server_time=current_time,cpu=cpu_percent,mem=mem_percent)
 
 @app.route('/status')
 def status():
